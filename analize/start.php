2e3df692b60bae5f8d2cd93850cb4d064bb8dfb3
Kirill Vagin <k.vagin@parcsis.org>
2014-03-31 12:17:58 +0400

    формальности

--- analize/start.php
+++ analize/start.php
@@ -4,8 +4,8 @@
  * @author k.vagin
  */
 
-$file = "/home/kirill/profiler__tmp_filePOPYdB.txt";
+$file = "/home/kirill/profiler__tmp.txt";
 
 $xlog = simplexml_load_file($file);
 
-print_r($xlog->FUNC_CALL[0]);
+//echo date('H:I:s', (int)$xlog->FUNC_CALL[0]->attributes()->timestamp), PHP_EOL;
