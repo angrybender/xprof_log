386575ebe6f7494332779cc4edcba7c334e69e0c
Kirill Vagin <k.vagin@parcsis.org>
2014-04-03 12:38:32 +0400

    db schema

--- docs/schema.sql
+++ docs/schema.sql
@@ -1,40 +1,33 @@
 CREATE TABLE IF NOT EXISTS `xhp_profiler`.`log_dumps` (
-  `id_dump` INT(11) NOT NULL,
-  `full_path` TEXT NULL DEFAULT NULL,
-  PRIMARY KEY (`id_dump`))
+  `id_dump` INT(11) not null primary key auto_increment,
+  `full_path` TEXT NULL DEFAULT NULL)
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8
 COLLATE = utf8_general_ci;
 
 CREATE TABLE IF NOT EXISTS `xhp_profiler`.`projects_files` (
-  `id_file` INT(11) NOT NULL,
-  `full_path` VARCHAR(1500) NULL DEFAULT NULL,
-  PRIMARY KEY (`id_file`))
+  `id_file` INT(11) not null primary key auto_increment,
+  `full_path` VARCHAR(1500) NULL DEFAULT NULL)
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8
 COLLATE = utf8_general_ci;
 
 CREATE TABLE IF NOT EXISTS `xhp_profiler`.`functions_call` (
-  `id_function` INT(11) NOT NULL,
+  `id_function` INT(11) not null primary key auto_increment,
   `timestamp` DOUBLE NULL DEFAULT NULL,
   `file` INT(11) NULL DEFAULT NULL,
   `line` INT(11) NULL DEFAULT NULL,
-  `dump` INT(11) NULL DEFAULT NULL,
-  PRIMARY KEY (`id_function`),
-  INDEX `function_of_dump_idx` (`dump` ASC),
-  INDEX `file_of_function_idx` (`file` ASC))
+  `dump` INT(11) NULL DEFAULT NULL)
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8
 COLLATE = utf8_general_ci;
 
 CREATE TABLE IF NOT EXISTS `xhp_profiler`.`vars_dump` (
-  `var_id` INT(11) NOT NULL,
+  `var_id` INT(11) not null primary key auto_increment,
   `function` INT(11) NULL DEFAULT NULL,
   `type` VARCHAR(20) NULL DEFAULT NULL,
   `assoc_key` VARCHAR(1000) NULL DEFAULT NULL,
-  `assoc_value` MEDIUMTEXT NULL DEFAULT NULL,
-  PRIMARY KEY (`var_id`),
-  INDEX `var_of_function_idx` (`function` ASC))
+  `assoc_value` MEDIUMTEXT NULL DEFAULT NULL)
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8
 COLLATE = utf8_general_ci;
